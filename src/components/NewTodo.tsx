a50a53679c1cc8fc39cdbe081286c2c11e8910f5
Jay Parmar <jay.parmar@biztechcs.lan>
2023-04-25 14:01:44 +0530

    WIP: Todo listing

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+    console.log(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor="todo-text">Todo text</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+      </div>
+      <button type="submit">Add TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
